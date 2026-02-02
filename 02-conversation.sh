73bc151b991cccede371f1aea976ad8a54fca3d8
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-02 15:40:46 +0530

    changed something

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,8 +2,8 @@
 
 PERSON1=Trumph
 PERSON2=putin
-echo "PERSON1:: Hello PERSON2 how are you?"
-echo "PERSON2:: Hi PERSON1, i am fine .how are you doing"
-echo "PERSON1:: doing fine, what are you learning PERSON2?"
-echo "PERSON2:: i am learning shell. what about you?"
+echo "$PERSON1:: Hello PERSON2 how are you?"
+echo "$PERSON2:: Hi PERSON1, i am fine .how are you doing"
+echo "$PERSON1:: doing fine, what are you learning PERSON2?"
+echo "$PERSON2:: i am learning shell. what about you?"
 
