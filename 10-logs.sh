bb47cd6eaca00f811b6bfdbb257a55a06f0e3e5a
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-03 19:38:57 +0530

    changed something

--- 10-logs.sh
+++ 10-logs.sh
@@ -5,34 +5,26 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    
-    echo "Please run this script with root user access"
-
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
-
 fi
 
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
-    if [ $USERID -ne 0 ]; then 
-
-         echo "$2....FAILURE" | tee -a $LOGS_FILE
-
-         exit 1
-
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
     else
-
-         echo "$2....SUCCESS"
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
     fi
-
-    dnf install nginx -y
-    VALIDATE $? "Installing Nginx"
-
-    dnf install mysql -y
-    VALIDATE $? "Installing mysql"
-
-    dnf install nodejs -y
-    VALIDATE $? "Instralling nodejs"
-
 }
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"
