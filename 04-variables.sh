e13bacb48c3d3e77a4064fc22d339b7d031ddd7b
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-03 14:58:20 +0530

    changed something

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,3 +3,10 @@
 START_TIME=$(date +%s)
 
 echo "script executed at : $START_TIME"s
+
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME-$START_TIME))
+
+echo "script executed in: $TOTAL_TIME
