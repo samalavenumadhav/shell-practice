c262cec8351c2b2a70713a47ec04e44c58ba1015
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-03 20:48:48 +0530

    changed something

---
+++ 12-set.sh
@@ -0,0 +1,24 @@
+#1/bin/bash
+
+set -e
+
+USERID=$(id -i)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ USERID -ne 0 ]; then
+   echo -e "R Please run this script wit root user access $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [$1 -ne 0];then
+       echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
+       exit 1
+
+    else
+       echo -e "$2....$G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
