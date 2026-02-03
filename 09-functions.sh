31b2f8ffa7d01e7d69371541ed879cc14360116a
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-03 18:34:11 +0530

    changed something

--- 09-functions.sh
+++ 09-functions.sh
@@ -2,14 +2,14 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ];then
+if [ $USERID -ne 0 ]; then
 
 echo "Please run this script with root user access"
 
 exit 1
 fi
 VALIDATE() {
-     if [$1 -ne 0];then
+     if [$1 -ne 0]; then
        echo "$2....FAILURE"
        exit 1
      else
