ed0efa9323b7584823e572020fb775e667e5258f
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-03 15:37:03 +0530

    changed something

---
+++ 07-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Pomo")
+
+echo "Fruits are: ${FRUITS[@]}"
+
+echo "First Fruit is: ${Fruits[0]}"
+
+echo "second Fruit is: ${Fruits[1]}"
+
+echo "Third Fruit is: ${Fruits[2]}"
