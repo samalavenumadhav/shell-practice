f829626f025d19602981acd2c3f3f81a41490a8b
samalavenumadhav <samalavenumadhav1999@gmail.com>
2026-02-03 20:59:46 +0530

    changed something

--- 12-set.sh
+++ 12-set.sh
@@ -1,27 +1,33 @@
-#1/bin/bash
+#!/bin/bash
 
-set -e
+set -e # this will be checking for errors, if errors it will exit
+
+#!/bin/bash
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-   echo -e "$R Please run this script wit root user access $N" | tee -a $LOGS_FILE
-   exit 1
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
+
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
-    if [$1 -ne 0];then
-       echo -e "$2....$R FAILURE $N" | tee -a $LOGS_FILE
-       exit 1
-
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
     else
-       echo -e "$2....$G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
+
 for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
 do
     dnf list installed $package &>>$LOGS_FILE
